5f409665e1f2fc4d5b1279eec933d36665ffd1c0
Nitin Singh Kanyal <77480195+Legit-Doodle@users.noreply.github.com>
2025-12-05 23:16:05 +0530

    Connect cosmos db (#1)
    * connect cosmos db and expose get all posts api

--- src/main/java/com/finscope/feed/entities/Post.java
+++ src/main/java/com/finscope/feed/entities/Post.java
@@ -1,22 +1,29 @@
 package com.finscope.feed.entities;
 
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Container(containerName = "post")
 public class Post {
-    private Long id;
+    @Id
+    private String id;
     private String title;
-    private String mediaUrl;
-    private List<String> tags;
-    private String content;
-    private Long postedBy;
-    private LocalDateTime createdAt;
-    private LocalDateTime lastUpdatedAt;
+    private String summary;
+    private String image_url;
+    private String source_name;
+    private String source_url;
+    private LocalDateTime published_at;
+    private LocalDateTime created_at;
+    private boolean is_verified;
+    @PartitionKey
+    private String partitionKey;
 }
--- src/main/java/com/finscope/feed/services/FeedService.java
+++ src/main/java/com/finscope/feed/services/FeedService.java
@@ -1,50 +1,17 @@
 package com.finscope.feed.services;
 
 import com.finscope.feed.entities.Post;
+import com.finscope.feed.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
+import reactor.core.publisher.Flux;
 
 @Service
 public class FeedService {
+    @Autowired
+    private PostRepository postRepository;
 
-    public List<Post> getFeed() {
-        // Return dummy list of posts for now
-        Post post1 = new Post(
-                1L,
-                "First Post",
-                "https://example.com/image1.jpg",
-                Arrays.asList("technology", "java", "spring"),
-                "This is the content of the first post",
-                101L,
-                LocalDateTime.now().minusDays(2),
-                LocalDateTime.now().minusDays(1)
-        );
-
-        Post post2 = new Post(
-                2L,
-                "Second Post",
-                "https://example.com/image2.jpg",
-                Arrays.asList("coding", "backend"),
-                "This is the content of the second post",
-                102L,
-                LocalDateTime.now().minusDays(1),
-                LocalDateTime.now()
-        );
-
-        Post post3 = new Post(
-                3L,
-                "Third Post",
-                null,
-                Arrays.asList("tutorial", "guide"),
-                "This is the content of the third post without media",
-                103L,
-                LocalDateTime.now().minusHours(5),
-                LocalDateTime.now().minusHours(5)
-        );
-
-        return Arrays.asList(post1, post2, post3);
+    public Flux<Post> getFeed() {
+        return postRepository.findAll();
     }
 }
