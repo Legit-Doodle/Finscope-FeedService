ac64d8e9e34479488cb09097d2827d629d016d13
Nitin Singh Kanyal <77480195+Legit-Doodle@users.noreply.github.com>
2025-12-06 18:06:36 +0530

    add azure app service configs (#2)

--- src/test/java/com/finscope/feed/FeedApplicationTests.java
+++ src/test/java/com/finscope/feed/FeedApplicationTests.java
@@ -3,7 +3,6 @@ package com.finscope.feed;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FeedApplicationTests {
 
 	@Test
