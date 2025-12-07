8088f91e116bb0ccbe016de65ecd95b968c3d938
Nitin Singh Kanyal <nitinkanyalofficial@gmail.com>
2025-12-07 10:44:47 +0530

    use reactive programming in feed controller

--- src/main/java/com/finscope/feed/controllers/FeedController.java
+++ src/main/java/com/finscope/feed/controllers/FeedController.java
@@ -1,14 +1,12 @@
 package com.finscope.feed.controllers;
 
 import com.finscope.feed.dto.FeedApiResponse;
-import com.finscope.feed.entities.Post;
 import com.finscope.feed.services.FeedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/feed")
@@ -17,14 +15,10 @@ public class FeedController {
     private FeedService feedService;
 
     @GetMapping
-    public FeedApiResponse getAllPosts() {
-        try {
-            List<Post> posts = feedService.getFeed().collectList().block();
-            long count = posts.size();
-            FeedApiResponse response = new FeedApiResponse(posts, count);
-            return response;
-        } catch (Exception e) {
-            throw new RuntimeException("Error fetching feed: " + e.getMessage(), e);
-        }
+    public Mono<FeedApiResponse> getAllPosts() {
+        return feedService.getFeed()
+                .collectList()
+                .map(posts -> new FeedApiResponse(posts, posts.size()))
+                .onErrorMap(e -> new RuntimeException("Error fetching feed: " + e.getMessage(), e));
     }
 }
